f5236817fb69669302bd4aa702498748c401e722
sandeep <sandeep@luckyinternationalschool.org>
2018-09-26 17:15:19 +0530

    this is my forst comit

---
+++ app/src/main/java/com/ietechnosoft/nik/lisworkbook/SplashActivity.java
@@ -0,0 +1,13 @@
+package com.ietechnosoft.nik.lisworkbook;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
